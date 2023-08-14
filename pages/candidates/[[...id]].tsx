4829af7e4a90de01d2bf2bb7409b4ae2e4e8815a
StacyKangethe <stacykangethe18@gmail.com>
2023-08-14 15:59:45 +0300

    GROUP CANDIDATES INIT

--- pages/candidates/[[...id]].tsx
+++ pages/candidates/[[...id]].tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { prisma } from '@/utils/connect'
 import { GetServerSideProps } from 'next'
 import { ElectionType } from '@/types/types';
@@ -8,18 +8,36 @@ type Props = {
 };
 
 function candidate({election}: Props){
+  const [values, setValues] = useState([])
+  const [options, setOptions] = useState()
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/election/").then((data)=>data.json()).then((val)=>setValues(val))
+  },[])
+
+  console.log(values,"values")
   return (
     <>
      <div>{election?.name}</div>
       <div className="w-screen overflow-x-scroll text-red-500">
         <div className="w-max flex">
-          {election?.candidates.map((candidate, index) => {
-          return(
-          <div key={index} className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]">
-            {candidate?.name}
-            {candidate?.position?.name}
-          </div>)
-          })}
+          
+            {
+               election?.candidates.map((candidate, index) =>
+               <label key={index}>{candidate?.position?.name}</label>) 
+               
+            }
+
+            
+          <select onChange={(e)=>setOptions(e.target.value)}>
+            {
+              election?.candidates.map((candidate, index) =>
+              <option key={candidate?.id}>{candidate?.name}</option>)
+            }
+          </select>
+          <div>
+            <h1>{options}</h1>
+          </div>
         </div>
       </div>
     </>
